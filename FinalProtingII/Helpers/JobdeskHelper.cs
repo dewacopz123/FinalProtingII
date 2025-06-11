cb3f81945bea542a23a40e1a0881bfacf01f063f
MUHAMMAD ZAKY AL-FARIZY <zakyfaryz@student.telkomuniversity.ac.id>
2025-06-11 07:20:32 +0700

    update jobdesk helper

--- FinalProtingII/Helpers/JobdeskHelper.cs
+++ FinalProtingII/Helpers/JobdeskHelper.cs
@@ -9,7 +9,8 @@ namespace FinalProtingII.Helpers
 
         public static List<Jobdesk> LoadJobdesk()
         {
-            if (!File.Exists(fileJobdeskPath)) return new List<Jobdesk>();
+            if (!File.Exists(fileJobdeskPath))
+                return new List<Jobdesk>();
 
             var json = File.ReadAllText(fileJobdeskPath);
             return JsonSerializer.Deserialize<List<Jobdesk>>(json) ?? new List<Jobdesk>();
